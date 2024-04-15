41c5a0657f00b40989e4eebfa6851a74e11d8280
fireworks <3245375160@qq.com>
2024-04-15 13:02:46 +0800

    Site updated: 2024-04-15 13:02:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/14/hello-world/"];function toRandomPost(){
+var posts=["2024/04/14/hello-world/","2024/04/15/test/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
