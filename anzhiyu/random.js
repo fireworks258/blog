ec7fcbdd50a5335955fa40168e568e5221565234
fireworks <3245375160@qq.com>
2024-04-16 12:30:13 +0800

    Site updated: 2024-04-16 12:30:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/14/hello-world/","2024/04/15/set集合/","2024/04/15/测试发布博文与标签/"];function toRandomPost(){
+var posts=["2024/04/15/测试发布博文与标签/","2024/04/14/hello-world/","2024/04/15/set集合/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
